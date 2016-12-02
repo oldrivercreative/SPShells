bbbaef88fd39f720c5fa557b0acd38dfbb93f1e4
Kyle Schaeffer <kyle@oldrivercreative.com>
2016-12-02 12:23:46 -0500

    Add SMTP test script

---
+++ Test-SMTP-Settings.ps1
@@ -0,0 +1,15 @@
+# source: http://jeffreypaarhuis.com/2013/02/12/send-test-email-from-sharepoint/
+
+Add-PSSnapin "Microsoft.SharePoint.Powershell" -ErrorAction SilentlyContinue
+
+# email configuration
+$email = "kyle@oldrivercreative.com"
+$subject = "SharePoint 2013 SMTP Test"
+$body = "This is a test email originating from the SharePoint 2013 web application server. Please disregard."
+
+# get web
+$site = New-Object Microsoft.SharePoint.SPSite "http://sharepoint"
+$web = $site.OpenWeb()
+
+# send
+[Microsoft.SharePoint.Utilities.SPUtility]::SendEmail($web, 0, 0, $email, $subject, $body)
